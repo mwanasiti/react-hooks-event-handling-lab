fb37a6d7d3c7dd1ee2b1aba124d8956ef413f581
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-07-16 19:05:01 +0300

    event

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function onFocus() {
+    console.log("Good!");
+  }
+
+  function onBlur() {
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <button onFocus={onFocus} onBlur={onBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+
+  function onChange() {
+    console.log("Entering password...")
+  }
+
+  return (
+    <input type="password" onChange={onChange} />
+  )
+}
+
+export default Keypad
